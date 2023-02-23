ba53d64cca48fd80de91fed8717d59d9ffc210fa
cdiazgranados <linacello@gmail.com>
2023-02-22 23:35:18 -0500

    part 10

--- src/main/java/io/zipcoder/interfaces/Teacher.java
+++ src/main/java/io/zipcoder/interfaces/Teacher.java
@@ -3,4 +3,6 @@ package io.zipcoder.interfaces;
 public interface Teacher {
     void teach(Learner learner, double numberOfHours);
     void lecture(Learner[] learners, double numberOfHours);
+
+    //hmm
 }
